2effe2b1cfaecc2183910ccafe31dde5e0401ac4
supoxiao <supoxiao@163.com>
2020-09-10 20:47:11 +0800

    167.-两数之和 II - 输入有序数组

---
+++ 数组/167.-两数之和 II - 输入有序数组.ts
@@ -0,0 +1,36 @@
+// 二分查找
+function twoSum(numbers: number[], target: number): number[] {
+  for (let i = 0; i < numbers.length; i++) {
+    let value = target - numbers[i]
+    let l = i + 1
+    let r = numbers.length - 1
+    // numbers[l,r]中找到value
+    while (l <= r) {
+      let mid = Math.floor((l + r) / 2)
+      if (numbers[mid] === value) {
+        return [i + 1, mid + 1]
+      } else if (numbers[mid] > value) {
+        r = mid - 1
+      } else {
+        l = mid + 1
+      }
+    }
+  }
+  return [-1, -1]
+}
+
+// 对撞指针
+function twoSum2(numbers: number[], target: number): number[] {
+  let i = 0
+  let j = numbers.length - 1
+  for (let k = 0; k < numbers.length; k++) {
+    if (numbers[i] + numbers[j] === target) {
+      return [i + 1, j + 1]
+    } else if (numbers[i] + numbers[j] > target) {
+      j--
+    } else {
+      i++
+    }
+  }
+  return [-1, -1]
+}
